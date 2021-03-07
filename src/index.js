00388cda611a6b4f11146d45e4d5ad08b6da602f
WFZ1 <pavelskumenko@gmail.com>
2021-03-07 09:56:25 +0200

    feat: add the solution which pass tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = (str, bracketsConfig) => {
+  if (str.length & 1) return false;
+
+  const brackets = bracketsConfig.map(el => el.join(''));
+
+  for (let i = 0; i < brackets.length; i++) {
+    if ( str.includes(brackets[i]) ) {
+      str = str.replace(brackets[i], '');
+      i = -1;
+    }
+  }
+
+  return !str;
 }
